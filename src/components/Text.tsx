3e616c423e79b80b9e141118fe440b3364fab2af
Yari Taft <yari.taft@vindow.com>
2021-07-17 03:00:54 -0300

    First commit. Rollup and stories working.

---
+++ src/components/Text.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Text() {
+    return <h1>Hello</h1>;
+  }
---
+++ src/stories/Text.stories.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import  Text  from '../components/Text';
+
+export default {
+  component: Text,
+  title: 'Components/Text',
+}
+
+export const Primary: React.VFC<{}> = () => <Text />;
