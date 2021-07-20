566def7d3ca072c8521dc0359033546d3a88cd3c
Yari Taft <yari.taft@gmail.com>
2021-07-19 23:42:40 -0300

    Webpack added, tested and working on shared components.

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const nodeExternals = require("webpack-node-externals");
+module.exports = {
+  entry: "./src/index.ts",
+  externals: [nodeExternals()],
+  devtool: "inline-source-map",
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "dist"),
+    libraryTarget: "commonjs",
+  },
+  plugins: [new CleanWebpackPlugin()],
+  resolve: {
+    extensions: [".tsx", ".ts", ".js", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: ["ts-loader"],
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+        include: path.resolve(__dirname, "./src"),
+      },
+    ],
+  },
+};
