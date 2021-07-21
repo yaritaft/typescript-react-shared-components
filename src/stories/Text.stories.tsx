b6a51051f5d259f79431faecfe1086c30e375f99
Yari Taft <yari.taft@gmail.com>
2021-07-21 18:58:32 -0300

    Removed babel and storybooks working again.2

--- src/stories/Text.stories.tsx
+++ src/stories/Text.stories.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React from "react";
 
-import  Text  from '../components/Text';
+import Text from "../components/Text";
 
 export default {
   component: Text,
-  title: 'Components/Text',
-}
+  title: "Components/Text",
+};
 
-export const Primary: React.VFC<{}> = () => <Text />;
+export const Primary: React.VFC<{}> = () => <Text num={4} />;
